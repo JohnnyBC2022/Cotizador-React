96f120f1afc44bf1a7101ca00cadc5d1598ed8e1
Johnny <jonathanbc2012@gmail.com>
2024-09-15 13:37:14 +0200

    header.jsx created

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <h1 className="text-4xl font-extrabold text-sky-800 text-center">
+      ¿Cuánto <span className="text-green-500">dinero</span> necesitas?
+    </h1>
+  );
+}
+
+export default Header;
